59c2dd287266655ab8b4973ffc7d72471ecadd7b
RutaMisiukeviciute <ruta.misiukeviute@gmail.com>
2024-02-14 09:04:54 +0200

    task 2

---
+++ Task 2/script.js
@@ -0,0 +1,11 @@
+/* ------------------------------ TASK 2 ----------------------------
+Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
+su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
+pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
+------------------------------------------------------------------- */
+document
+  .querySelector("#btn__element")
+  .addEventListener("click", () => {
+    const text = document.querySelector("#btn__state");
+    text.textContent++;
+  });
