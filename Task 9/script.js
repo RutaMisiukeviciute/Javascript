b9be4c697bb1bd9f294e3a1f939c42eafae24199
RutaMisiukeviciute <ruta.misiukeviute@gmail.com>
2024-02-14 11:06:49 +0200

    task 9

---
+++ Task 9/script.js
@@ -0,0 +1,22 @@
+/* ------------------------------ TASK 9 ---------------------------------------------------------------
+Sukurkite klasę "Movie", kuri sukuria objektus su 3 savybėm ir 1 metodu:
+
+Savybės:
+title, director, budget
+Metodas: 
+wasExpensive() - jeigu filmo budget bus didesnis nei 100 000 000 mln USD, tada gražins true, kitu atveju false.
+------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = budget;
+  }
+  wasExpensive() {
+    if (this.budget > 100000000) {
+      return true;
+    }
+    else return false;
+  }
+}
