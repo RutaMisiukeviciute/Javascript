94973d2e42aaef667c102a2b9dec4fd45642b60d
RutaMisiukeviciute <ruta.misiukeviute@gmail.com>
2024-02-14 10:45:20 +0200

    task 6

---
+++ Task 6/script.js
@@ -0,0 +1,31 @@
+/* ------------------------------ TASK 6 -----------------------------------
+Turimas "users" masyvas. 
+
+Parašykite funkcijas, kurios atliks nurodytas užduotis:
+1. funkcija "getUserAverageAge" - kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins visų "users" amžiaus vidurkį kaip skaičių.
+2. funkcija "getUsersNames" -  kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann Smith'..].
+-------------------------------------------------------------------------- */
+
+const users = [
+  { id: '1', name: 'John Smith', age: 20 },
+  { id: '2', name: 'Ann Smith', age: 24 },
+  { id: '3', name: 'Tom Jones', age: 31 },
+  { id: '4', name: 'Rose Peterson', age: 17 },
+  { id: '5', name: 'Alex John', age: 25 },
+  { id: '6', name: 'Ronald Jones', age: 63 },
+  { id: '7', name: 'Elton Smith', age: 16 },
+  { id: '8', name: 'Simon Peterson', age: 30 },
+  { id: '9', name: 'Daniel Cane', age: 51 },
+];
+
+function getUserAverageAge(masAvg) {
+  let sum = masAvg.reduce((acc, curr) => { return acc + curr.age }, 0);
+  return sum / masAvg.length;
+}
+
+function getUsersNames(masName) {
+  let names = masName.map(el => { return el.name });
+  return names;
+}
